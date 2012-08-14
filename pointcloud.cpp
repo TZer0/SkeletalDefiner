8c5fee5ad91a0e5123be4bb909b99b29d0173728
TZer0 <tzer0.jan@gmail.com>
2012-08-14 16:58:13 +0200

    "Working" selection.

--- pointcloud.cpp
+++ pointcloud.cpp
@@ -12,8 +12,21 @@ void PointCloud::setBound(float *far) {
 
 void PointCloud::selectNearestPoint(QVector3D direction, QVector3D start) {
 	QVector3D origStart = start;
+	bool found = false;
 	while ((origStart - start).length() < *far) {
-		start += direction;
-		qDebug() << start;
+		start += direction*0.05;
+		for (int i = 0; i < points.length(); i++) {
+			if ((*points[i]-start).length() < 0.1) {
+				found = true;
+				if (!selected.contains(points[i])) {
+					selected.append(points[i]);
+				}
+				qDebug() << "found.";
+				break;
+			}
+		}
+		if (found) {
+			break;
+		}
 	}
 }
